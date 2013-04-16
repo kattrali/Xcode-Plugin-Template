d74367ecb931c0967078b111f60b3583a30e3658
Delisa Mason <iskanamagus@gmail.com>
2013-04-16 15:49:13 -0400

    Initial Commit

---
+++ ___PACKAGENAME___.h
@@ -0,0 +1,13 @@
+//
+//  ___PACKAGENAME___.h
+//  ___PACKAGENAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import <AppKit/AppKit.h>
+
+@interface ___PACKAGENAME___ : NSObject
+
+@end
---
+++ ___PACKAGENAME___.m
@@ -0,0 +1,53 @@
+//
+//  ___PACKAGENAME___.m
+//  ___PACKAGENAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import "___PACKAGENAME___.h"
+
+@implementation ___PACKAGENAME___
+
+
++ (void)pluginDidLoad:(NSBundle *)plugin
+{
+    static id sharedPlugin = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedPlugin = [[self alloc] init];
+    });
+}
+
+- (id)init
+{
+    if (self = [super init]) {
+        // Create menu items, initialize UI, etc.
+
+        // Sample Menu Item:
+        NSMenuItem *viewMenuItem = [[NSApp mainMenu] itemWithTitle:@"File"];
+        if (viewMenuItem) {
+            [[viewMenuItem submenu] addItem:[NSMenuItem separatorItem]];
+            NSMenuItem *sample = [[[NSMenuItem alloc] initWithTitle:@"Do Action" action:@selector(doMenuAction) keyEquivalent:@""] autorelease];
+            [sample setTarget:self];
+            [[viewMenuItem submenu] addItem:sample];
+        }
+    }
+    return self;
+}
+
+// Sample Action, for menu item:
+- (void) doMenuAction
+{
+    NSAlert *alert = [NSAlert alertWithMessageText:@"Hello, World" defaultButton:nil alternateButton:nil otherButton:nil informativeTextWithFormat:@""];
+    [alert runModal];
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    [super dealloc];
+}
+
+@end
