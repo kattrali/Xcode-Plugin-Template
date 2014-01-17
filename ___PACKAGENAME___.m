26bea7be7f4bd91ea3eebb97a04eab700917aef8
David Martin <cerberillo@users.noreply.github.com>
2014-01-17 09:23:53 +0100

    Update ___PACKAGENAME___.m
    Remove double open bracket

--- ___PACKAGENAME___.m
+++ ___PACKAGENAME___.m
@@ -30,7 +30,6 @@ static ___PACKAGENAME___ *sharedPlugin;
 }
 
 - (id)initWithBundle:(NSBundle *)plugin {
-{
     if (self = [super init]) {
         // reference to plugin's bundle, for resource acccess
         self.bundle = plugin;
