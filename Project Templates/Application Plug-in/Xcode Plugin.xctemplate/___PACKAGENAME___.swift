0296c652fc0dbce63874900e47845d6e461b918e
Marko Tadić <tadija@me.com>
2015-07-24 17:27:42 +0200

    Fixed crash when using Swift template in Xcode 6.4

--- Project Templates/Application Plug-in/Xcode Plugin.xctemplate/___PACKAGENAME___.swift
+++ Project Templates/Application Plug-in/Xcode Plugin.xctemplate/___PACKAGENAME___.swift
@@ -10,20 +10,28 @@ import AppKit
 var sharedPlugin: ___PACKAGENAME___?
 
 class ___PACKAGENAME___: NSObject {
+
     var bundle: NSBundle
+    lazy var center = NSNotificationCenter.defaultCenter()
 
     init(bundle: NSBundle) {
         self.bundle = bundle
 
         super.init()
-        createMenuItems()
+        center.addObserver(self, selector: Selector("createMenuItems"), name: NSApplicationDidFinishLaunchingNotification, object: nil)
     }
 
     deinit {
-        NSNotificationCenter.defaultCenter().removeObserver(self)
+        removeObserver()
+    }
+
+    func removeObserver() {
+        center.removeObserver(self)
     }
 
     func createMenuItems() {
+        removeObserver()
+
         var item = NSApp.mainMenu!!.itemWithTitle("Edit")
         if item != nil {
             var actionMenuItem = NSMenuItem(title:"Do Action", action:"doMenuAction", keyEquivalent:"")
